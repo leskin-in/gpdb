9e8b877a704fd51e0a01423a0371cd46bcc94771
Ivan Leskin <leskin.in@arenadata.io>
2019-07-31 13:42:35 -0700

    Use xact_desc in all cases of unit test of access/rmgrdesc
    Proposed by Adam Berlin <aberlin@pivotal.io>

--- src/backend/access/rmgrdesc/test/xactdesc_test.c
+++ src/backend/access/rmgrdesc/test/xactdesc_test.c
@@ -26,9 +26,9 @@ test_xactdescprepareCommit(void **state)
 	tpfh->tablespace_oid_to_delete_on_commit = 42;
 	tpfh->tablespace_oid_to_delete_on_abort = InvalidOid;
 
-	xact_desc_prepare(buf, rec);
+	xact_desc(buf, rec);
 
-	assert_string_equal("at = 2019-07-30 18:26:11.83003+00; gid = 4242424242-0000000042; tablespace_oid_to_delete_on_commit = 42", buf->data);
+	assert_string_equal("prepare: at = 2019-07-30 18:26:11.83003+00; gid = 4242424242-0000000042; tablespace_oid_to_delete_on_commit = 42", buf->data);
 }
 
 void
@@ -45,9 +45,9 @@ test_xactdescprepareAbort(void **state)
 	tpfh->tablespace_oid_to_delete_on_commit = InvalidOid;
 	tpfh->tablespace_oid_to_delete_on_abort = 42;
 
-	xact_desc_prepare(buf, rec);
+	xact_desc(buf, rec);
 
-	assert_string_equal("at = 2019-07-30 18:26:11.83003+00; gid = 4242424242-0000000042; tablespace_oid_to_delete_on_abort = 42", buf->data);
+	assert_string_equal("prepare: at = 2019-07-30 18:26:11.83003+00; gid = 4242424242-0000000042; tablespace_oid_to_delete_on_abort = 42", buf->data);
 }
 
 void
@@ -64,28 +64,9 @@ test_xactdescprepareNone(void **state)
 	tpfh->tablespace_oid_to_delete_on_commit = InvalidOid;
 	tpfh->tablespace_oid_to_delete_on_abort = InvalidOid;
 
-	xact_desc_prepare(buf, rec);
-
-	assert_string_equal("at = 2019-07-30 18:26:11.83003+00; gid = 4242424242-0000000042", buf->data);
-}
-
-void
-test_xactdescPrepare(void **state)
-{
-	StringInfo buf = makeStringInfo();
-	XLogRecord *rec = palloc(SizeOfXLogRecord + MAXALIGN(sizeof(TwoPhaseFileHeader)));
-
-	rec->xl_info = XLOG_XACT_PREPARE;
-	TwoPhaseFileHeader* tpfh = (TwoPhaseFileHeader*) XLogRecGetData(rec);
-
-	tpfh->prepared_at = 617826371830030;
-	strcpy(tpfh->gid, "4242424242-0000000042");
-	tpfh->tablespace_oid_to_delete_on_commit = 42;
-	tpfh->tablespace_oid_to_delete_on_abort = InvalidOid;
-
 	xact_desc(buf, rec);
 
-	assert_string_equal("prepare: at = 2019-07-30 18:26:11.83003+00; gid = 4242424242-0000000042; tablespace_oid_to_delete_on_commit = 42", buf->data);
+	assert_string_equal("prepare: at = 2019-07-30 18:26:11.83003+00; gid = 4242424242-0000000042", buf->data);
 }
 
 int
@@ -99,8 +80,7 @@ main(int argc, char* argv[])
 	const UnitTest tests[] = {
 		unit_test(test_xactdescprepareCommit),
 		unit_test(test_xactdescprepareAbort),
-		unit_test(test_xactdescprepareNone),
-		unit_test(test_xactdescPrepare)
+		unit_test(test_xactdescprepareNone)
 	};
 
 	return run_tests(tests);
